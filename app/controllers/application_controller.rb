efdbed5360c635a7302a742a244d61c5b9650008
Julien Pierlot <pierlot.julien@gmail.com>
2021-12-17 17:46:31 +0100

    add github actions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::API
-  def default_url_options
-    { host: ENV['HOSTNAME'] }
-  end
+
 end
--- config/application.rb
+++ config/application.rb
@@ -36,5 +36,7 @@ module BridgeApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    Rails.application.routes.default_url_options[:host] = ENV['HOSTNAME']
   end
 end
